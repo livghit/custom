6bb114614f84782bb21c6e575893b78b4aed7d6d
Ghita <liviughita@st-elisabeth-stiftung.de>
2023-05-09 15:11:05 +0200

    Project initiated

---
+++ mappings.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.abc = {
+  n = {
+    ["<leader>a"] = { ":lua require('harpoon.mark').add_file() <CR>",
+      "Harpoon add file"
+    },
+
+    ["<S-e>"] = {
+      ":lua require('harpoon.ui').toggle_quick_menu() <CR>",
+      "Harpoon show files"
+    }
+  }
+}
+
+
+return M
---
+++ plugins.lua
@@ -0,0 +1,40 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "intelephense",
+        "tailwindcss-language-server",
+        "typescript-language-server ",
+        "html-lsp",
+        "css-lsp",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+  }
+}
+return plugins
